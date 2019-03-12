a140af1968c7a1720a9e00c4f757f25848880c18
Ivo Angelov <jagdcake@tuta.io>
2019-03-12 20:46:11 +0200

    Create a method to handle errors
    It prints the error message and returns the type that the app expects.

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 
 import { User } from './user';
 
@@ -10,6 +10,14 @@ import { User } from './user';
 export class UserService {
 
     private usersUrl = 'http://127.0.0.1:8000/mock_users';
+
+    private handleError<T>(newResult ?: T) {
+        return (error: any): Observable<T> => {
+            console.error(error.message);
+
+            return of(newResult as T);
+        };
+    }
 
     getUsers(): Observable<User[]> {
         return this.http.get<User[]>(this.usersUrl);
