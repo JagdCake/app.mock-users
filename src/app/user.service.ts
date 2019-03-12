d61174234fc5b4c17d9bbd046a328521afd28616
Ivo Angelov <jagdcake@tuta.io>
2019-03-12 16:02:28 +0200

    Create a method to get user data from the API

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+import { User } from './user';
 
 @Injectable({
   providedIn: 'root'
@@ -7,6 +10,10 @@ import { HttpClient } from '@angular/common/http';
 export class UserService {
 
     private usersUrl = 'http://127.0.0.1:8000/mock_users';
+
+    getUsers(): Observable<User[]> {
+        return this.http.get<User[]>(this.usersUrl);
+    }
 
     constructor(private http: HttpClient) { }
 }
