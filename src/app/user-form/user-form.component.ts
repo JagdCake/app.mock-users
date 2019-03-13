7f8ed38a7637b31f24abeaebe6a8d517a949e3a8
Ivo Angelov <jagdcake@tuta.io>
2019-03-13 15:19:15 +0200

    Create a form group property for a user

--- src/app/user-form/user-form.component.ts
+++ src/app/user-form/user-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-user-form',
@@ -6,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-form.component.css']
 })
 export class UserFormComponent implements OnInit {
+
+    user = new FormGroup({
+        first_name: new FormControl(''),
+        last_name: new FormControl(''),
+        age: new FormControl(''),
+        sex: new FormControl(''),
+    });
 
   constructor() { }
 
