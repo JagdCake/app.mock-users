5af084da025c3200aea690ea92241941e7c6e91b
Ivo Angelov <jagdcake@tuta.io>
2019-03-15 19:32:31 +0200

    Lower the number of user cards per page

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -43,7 +43,7 @@ export class DashboardComponent implements OnInit {
     userId = null;
     name: string;
 
-    itemsPerPage = 24;
+    itemsPerPage = 18;
     page: number;
     pages: number;
 
