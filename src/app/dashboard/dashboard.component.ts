59e46a268daf4de671bdfecfaa9135a671f19b9c
Ivo Angelov <jagdcake@tuta.io>
2019-03-15 08:25:04 +0200

    Create a method which sets properties needed to confirm deleting a user

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -12,15 +12,24 @@ import { ApiResponse } from '../api-response';
 export class DashboardComponent implements OnInit {
 
     users: User[] = [];
+    userId = null;
+    name: string;
 
     getUsers(): void {
         this.userService.getUsers()
             .subscribe((users) => this.users = users);
     }
 
-    deleteUser(id: number): void {
-        this.userService.deleteUser(id)
+    deleteUser(): void {
+        this.userService.deleteUser(this.userId)
             .subscribe((response: ApiResponse) => console.log(response.message));
+
+        this.userId = 'deleted';
+    }
+
+    confirmDelete(id: number, name: string) {
+        this.userId = id;
+        this.name = name;
     }
 
     constructor(private userService: UserService) { }
