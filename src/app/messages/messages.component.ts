060177d3d6b02b9bca98eb843dfec6fa26e9100a
Ivo Angelov <jagdcake@tuta.io>
2019-03-15 11:18:03 +0200

    Import and declare a property for the message service

--- src/app/messages/messages.component.ts
+++ src/app/messages/messages.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MessagesService } from '../messages.service';
 
 @Component({
   selector: 'app-messages',
@@ -7,9 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MessagesComponent implements OnInit {
 
-  constructor() { }
+    constructor(private messageService: MessagesService) { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
 }
