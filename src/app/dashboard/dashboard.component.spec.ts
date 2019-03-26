50b4eed5de0a7d6cf06cfbbd03e136a831725703
Ivo Angelov <jagdcake@tuta.io>
2019-03-26 12:15:39 +0200

    Simplify test and remove import that shouldn't be necessary for testing

--- src/app/dashboard/dashboard.component.spec.ts
+++ src/app/dashboard/dashboard.component.spec.ts
@@ -1,31 +1,27 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
-import { HttpClientModule } from '@angular/common/http';
 
 import { DashboardComponent } from './dashboard.component';
 
 describe('DashboardComponent', () => {
-    let component: DashboardComponent;
     let fixture: ComponentFixture<DashboardComponent>;
+    let dashboard: DashboardComponent;
+    let elements;
+    let debugElements;
 
     beforeEach(async(() => {
         TestBed.configureTestingModule({
-            imports: [
-                RouterTestingModule,
-                HttpClientModule,
-            ],
-          declarations: [ DashboardComponent ]
-        })
-        .compileComponents();
+            imports: [ RouterTestingModule ],
+            declarations: [ DashboardComponent ],
+        }).compileComponents();
+
+        fixture = TestBed.createComponent(DashboardComponent);
+        dashboard = fixture.componentInstance;
+        elements = fixture.nativeElement;
+        debugElements = fixture.debugElement;
     }));
 
-    beforeEach(() => {
-        fixture = TestBed.createComponent(DashboardComponent);
-        component = fixture.componentInstance;
-        fixture.detectChanges();
-    });
-
-    it('should create', () => {
-        expect(component).toBeTruthy();
+    it('should create the dashboard', () => {
+        expect(dashboard).toBeTruthy();
     });
 });
