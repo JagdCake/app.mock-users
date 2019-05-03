e4be835cbbab3f6a1fc2d23298c2c9b71a07c1da
Ivo Angelov <jagdcake@tuta.io>
2019-05-03 14:17:10 +0300

    Test if the pagination service redirects on empty page

--- src/app/pagination.service.spec.ts
+++ src/app/pagination.service.spec.ts
@@ -1,15 +1,38 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { Router } from '@angular/router';
 
 import { PaginationService } from './pagination.service';
 
 describe('PaginationService', () => {
-    beforeEach(() => TestBed.configureTestingModule({
-        imports: [ RouterTestingModule ],
-    }));
+    let service: PaginationService;
+    let router: Router;
+
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            imports: [ RouterTestingModule ],
+            providers: [ PaginationService ],
+        });
+
+        service = TestBed.get(PaginationService);
+        router = TestBed.get(Router);
+    });
 
     it('should be created', () => {
-        const service: PaginationService = TestBed.get(PaginationService);
         expect(service).toBeTruthy();
+    });
+
+    it('should redirect if there is nothing on a page', () => {
+        const navigationSpy = spyOn(router, 'navigateByUrl');
+
+        // arg1 = current page number, arg2 = total number of pages
+        service.redirectIfNothingOnPage(1, 1);
+        expect(navigationSpy).not.toHaveBeenCalled();
+
+        service.redirectIfNothingOnPage(0, 1);
+        expect(navigationSpy).toHaveBeenCalledWith('/');
+
+        service.redirectIfNothingOnPage(2, 1);
+        expect(navigationSpy).toHaveBeenCalledWith('/');
     });
 });
