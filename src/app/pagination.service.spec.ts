7145a832587406f5a93bb100591c429d014a39a3
Ivo Angelov <jagdcake@tuta.io>
2019-05-03 13:16:06 +0300

    Update pagination test

--- src/app/pagination.service.spec.ts
+++ src/app/pagination.service.spec.ts
@@ -1,12 +1,15 @@
 import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { PaginationService } from './pagination.service';
 
 describe('PaginationService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+    beforeEach(() => TestBed.configureTestingModule({
+        imports: [ RouterTestingModule ],
+    }));
 
-  it('should be created', () => {
-    const service: PaginationService = TestBed.get(PaginationService);
-    expect(service).toBeTruthy();
-  });
+    it('should be created', () => {
+        const service: PaginationService = TestBed.get(PaginationService);
+        expect(service).toBeTruthy();
+    });
 });
