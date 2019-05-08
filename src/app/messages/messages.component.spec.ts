f0b85e49c1e6939616abbd4512bedf91aea63cba
Ivo Angelov <jagdcake@tuta.io>
2019-05-08 15:53:38 +0300

    Test if error messages are displayed
    Also, display the message body on a new line.

--- src/app/messages/messages.component.spec.ts
+++ src/app/messages/messages.component.spec.ts
@@ -1,25 +1,37 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MessagesComponent } from './messages.component';
+import { MessagesService } from '../messages.service';
 
 describe('MessagesComponent', () => {
     let component: MessagesComponent;
     let fixture: ComponentFixture<MessagesComponent>;
+    let elements: any;
+    let service: MessagesService;
 
     beforeEach(async(() => {
         TestBed.configureTestingModule({
-          declarations: [ MessagesComponent ]
+          declarations: [ MessagesComponent ],
+            providers: [ MessagesService ],
         })
         .compileComponents();
-    }));
 
-    beforeEach(() => {
         fixture = TestBed.createComponent(MessagesComponent);
         component = fixture.componentInstance;
-        fixture.detectChanges();
-    });
+        elements = fixture.debugElement.nativeElement;
+        service = TestBed.get(MessagesService);
+    }));
 
     it('should create the messages component', () => {
         expect(component).toBeTruthy();
+    });
+
+
+    it('should display error messages', () => {
+        service.log('error', 'Something happened');
+        fixture.detectChanges();
+
+        const successMessage = elements.querySelector('.error-message').textContent;
+        expect(successMessage).toContain('Something happened');
     });
 });
